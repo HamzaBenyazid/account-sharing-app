d6b6de3d9aa9cbf1185bbec2dac35dfbbe5d0314
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-13 16:19:34 +0100

    initialized offer + user exceptions

--- src/main/java/com/winchesters/accountsharingapp/offer/OfferRepository.java
+++ src/main/java/com/winchesters/accountsharingapp/offer/OfferRepository.java
@@ -1,4 +1,8 @@
 package com.winchesters.accountsharingapp.offer;
 
-public interface OfferRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer,Long> {
 }
