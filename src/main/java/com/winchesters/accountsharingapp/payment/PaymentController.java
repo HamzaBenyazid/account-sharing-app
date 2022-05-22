c85a38586bc8794eae75531970a345865e313fd5
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-22 14:13:34 +0100

    init payment

--- src/main/java/com/winchesters/accountsharingapp/payment/PaymentController.java
+++ src/main/java/com/winchesters/accountsharingapp/payment/PaymentController.java
@@ -1,9 +1,12 @@
 package com.winchesters.accountsharingapp.payment;
 
+import com.winchesters.accountsharingapp.account.AccountService;
 import com.winchesters.accountsharingapp.offer.Offer;
 import com.winchesters.accountsharingapp.offer.OfferService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,7 @@ import javax.validation.Valid;
 @RequestMapping(path = "api/payment")
 @RequiredArgsConstructor
 public class PaymentController {
+    private static final Logger Log = LoggerFactory.getLogger(PaymentController.class);
 //    private final PaymentService paymentService;
 
     @GetMapping("/test")
--- src/main/java/com/winchesters/accountsharingapp/payment/PaymentService.java
+++ src/main/java/com/winchesters/accountsharingapp/payment/PaymentService.java
@@ -1,4 +1,16 @@
 package com.winchesters.accountsharingapp.payment;
 
+import com.winchesters.accountsharingapp.account.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
 public class PaymentService {
+    private static final Logger Log = LoggerFactory.getLogger(PaymentService.class);
 }
