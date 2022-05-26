b0f3a24e77de1686160927e30e7ef6756797bebc
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-27 00:55:01 +0100

    testing jenkins

--- script.groovy
+++ script.groovy
@@ -3,14 +3,14 @@ def buildJar(){
     sh 'mvn clean package'
 }
 def buildImage(){
-//    echo "${IMAGE_TAG}" //${IMAGE_TAG}
-    sh "docker build --target build -t oubaydos/temp:tmm ."
+    echo "${IMAGE_NAME}"
+    sh "docker build -t oubaydos/temp:${IMAGE_NAME} ."
 }
 def pushImage(){
     echo "pushing the docker image"
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push oubaydos/temp:$IMAGE_TAG"
+        sh "docker push oubaydos/temp:$IMAGE_NAME"
     }
 }
 def commitVersion(){
