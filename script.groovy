69a8abb0cf02ca14ef722a803806fe02e5b31c18
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-05-27 01:42:42 +0100

    updates script.groovy

--- script.groovy
+++ script.groovy
@@ -3,24 +3,24 @@ def buildJar(){
     sh 'mvn clean package'
 }
 def buildImage(){
-    echo "${IMAGE_NAME}"
-    sh "docker build -t oubaydos/temp:${IMAGE_NAME} ."
+    echo "${IMAGE_TAG}"
+    sh "docker build -t oubaydos/temp:${IMAGE_TAG} ."
 }
 def pushImage(){
     echo "pushing the docker image"
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push oubaydos/temp:$IMAGE_NAME"
+        sh "docker push oubaydos/temp:$IMAGE_TAG"
     }
 }
 def commitVersion(){
-    withCredentials([usernamePassword(credentialsId: 'gitlab', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
+    withCredentials([usernamePassword(credentialsId: 'githubWithToken', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
         sh "git config user.name 'jenkins'"
         sh "git config user.email 'jenkins@oubaydos.com'"
-        sh "git remote set-url origin https://${USERNAME}:${PASSWORD}@github.com/Coaching-Website-Team/Coaching-Website"
+        sh "git remote set-url origin https://${PASSWORD}@github.com/HamzaBenyazid/account-sharing-app"
         sh "git add . "
         sh "git commit -m 'update version'"
-        sh "git push origin HEAD:master"
+        sh "git push origin HEAD:${BRANCH_NAME}"
     }
 }
 return this
